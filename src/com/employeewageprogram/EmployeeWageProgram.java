a4de06e3414a5429534e0e2fc8fc0eb1a4440a6f
moin21 <mamoinuddin@gmail.com>
2022-02-15 14:06:40 +0530

    Added: Logic for part time employee and wage

--- src/com/employeewageprogram/EmployeeWageProgram.java
+++ src/com/employeewageprogram/EmployeeWageProgram.java
@@ -1,22 +1,27 @@
 package com.employeewageprogram;
 
 public class EmployeeWageProgram {
-	public static int empAttendance = (int) (Math.floor(Math.random() * 10) % 2);
+	public static int empAttendance = (int) (Math.floor(Math.random() * 10) % 3);
 	public static final int WAGE_PER_HOUR = 20;
 	public static final int FULL_TIME_HOUR = 8;
+	public static final int PART_TIME_HOUR = 4;
 	public static int dailyWage;
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Program!!");
 		attendanceWageCalculation();
 	}
+
 	public static void attendanceWageCalculation() {
-		if(empAttendance == 0) {
+		if (empAttendance == 0) {
 			dailyWage = 0;
-			System.out.println("Employee is absent today. Total wage is "+ dailyWage);
-		}
-		else {
+			System.out.println("Employee is absent today. Total wage is " + dailyWage);
+		} else if (empAttendance == 1) {
+			dailyWage = WAGE_PER_HOUR * PART_TIME_HOUR;
+			System.out.println("Part Time Employee is present today. Total wage is " + dailyWage);
+		} else {
 			dailyWage = WAGE_PER_HOUR * FULL_TIME_HOUR;
-			System.out.println("Employee is present today. Total wage is "+ dailyWage);
-	}
+			System.out.println("Full Time Employee is present today. Total wage is " + dailyWage);
+		}
 	}
 }
