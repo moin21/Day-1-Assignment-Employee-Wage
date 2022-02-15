175a820a35ed05564c3c14260cee5dcf5cc102ec
moin21 <mamoinuddin@gmail.com>
2022-02-15 13:42:30 +0530

    Added: Welcome message

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a program to calculate Employee Wage.
---
+++ src/com/employeewageprogram/EmployeeWageProgram.java
@@ -0,0 +1,7 @@
+package com.employeewageprogram;
+
+public class EmployeeWageProgram {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Program!!");
+	}
+}
