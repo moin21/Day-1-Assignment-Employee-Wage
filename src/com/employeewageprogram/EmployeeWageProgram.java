bc467fdf342e4cbefe668cbcfc7eb262db8c4696
moin21 <mamoinuddin@gmail.com>
2022-02-15 13:48:17 +0530

    Added: Logic for employee attendance

--- src/com/employeewageprogram/EmployeeWageProgram.java
+++ src/com/employeewageprogram/EmployeeWageProgram.java
@@ -1,7 +1,15 @@
 package com.employeewageprogram;
 
 public class EmployeeWageProgram {
+	public static int empAttendance = (int) (Math.floor(Math.random() * 10) % 2);
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Program!!");
+		attendance();
+	}
+	public static void attendance() {
+		if(empAttendance == 0) {
+			System.out.println("Employee is absent today.");
+		}
+		else System.out.println("Employee is present today.");
 	}
 }
